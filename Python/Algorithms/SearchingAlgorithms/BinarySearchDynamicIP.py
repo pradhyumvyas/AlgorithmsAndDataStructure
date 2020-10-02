82c7f225b23b6a205f649b882e2df69275073635
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-10-02 23:40:29 +0530

    Added Binary search with dynamic input

---
+++ Python/Algorithms/SearchingAlgorithms/BinarySearchDynamicIP.py
@@ -0,0 +1,32 @@
+size = int(input("Enter a Element of array"))
+array = []
+
+print("Enter a elements in sorted order")
+
+for i in range(size):
+    temp = int(input())
+    array.append(temp)
+
+array.sort()
+
+low = 0
+high = size-1
+
+search = int(input("Enter a number to be searched"))
+
+while low <= high:
+
+    mid = low + (high-low)//2
+
+    if array[mid] == search:
+        print("Element found at " + str(mid) + " index")
+        break
+    elif array[mid] < search:
+        low = mid+1
+
+    else:
+        high = mid-1
+else:
+    print("Element are not present")
+
+print("Index start with 0 ")
